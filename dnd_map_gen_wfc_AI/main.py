6847195823cdb1579ffecc822fdb296fba2c0da4
artofghost <404368@student.fontys.nl>
2023-06-09 21:50:51 +0200

    fk this

--- dnd_map_gen_wfc_AI/main.py
+++
@@ -1,7 +0,0 @@
-class Cell:
-    def __init__(self, value):
-        self.collapsed = False
-        if isinstance(value, list):
-            self.options = value
-        else:
-            self.options = [i for i in range(value)]
